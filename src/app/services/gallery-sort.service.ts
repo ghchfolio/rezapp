86469a96e9925b3ada9261e381dd6ab571053215
CH Folio <ghchfolio@gmail.com>
2024-02-05 13:26:17 -0600

    remove unused

--- src/app/services/gallery-sort.service.ts
+++ src/app/services/gallery-sort.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, signal } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { ISample } from '../models/sample.model';
 
 @Injectable({
@@ -10,44 +9,10 @@ export class GallerySortService {
     sortDir: string = 'des';
     samples$$ = signal(this.dataStore);
 
-    #sortDirSubj = new BehaviorSubject<string>(this.sortDir);
-    #sortDir$ = this.#sortDirSubj.asObservable();
-
-    subs = this.#sortDir$.subscribe(dir => {
-        this.sortDir = dir;
-        if (this.dataStore.length) this.sortByYear();
-    });
-
-    setSortDirection(sortDir: string) {
-        this.#sortDirSubj.next(sortDir);
-    }
-
     setDataSource(data: ISample[]) {
         if (data.length) {
-            this.dataStore = data;
-            this.sortByYear();
+            this.dataStore = data.sort((a: ISample, b: ISample) => (a.year > b.year) ? -1 : 1);
+            this.samples$$.update(() => JSON.parse(JSON.stringify(this.dataStore)));
         }
-    }
-
-    sortByYear() {
-        if (this.sortDir === 'des') {
-            this.dataStore.sort(function (a: ISample, b: ISample) {
-                if (a.year > b.year) return -1;
-                if (b.year < a.year) return 1;
-                return 0;
-            });
-        } else {
-            this.dataStore.sort(function (a: ISample, b: ISample) {
-                if (a.year < b.year) return -1;
-                if (b.year > a.year) return 1;
-                return 0;
-            });
-        }
-
-        this.samples$$.update(() => JSON.parse(JSON.stringify(this.dataStore)));
-    }
-
-    ngOnDestroy() {
-        this.subs.unsubscribe();
     }
 }
